487336410f7222bccb696b180680f04215d7a10c
Eli Kanavaros <xanelik@gmail.com>
2023-03-12 15:48:42 -0700

    initializing repo

---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+const Nav = () => {
+  const linkGiver = () => {
+    onclick = (e) => {};
+  };
+
+  return (
+    <div>
+      <button>HOME</button>
+      <button>GALLERY</button>
+      <button>CONTACT</button>
+    </div>
+  );
+};
+
+export default Nav;
